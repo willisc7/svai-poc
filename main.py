319d5f779101ee6c6a842ac56182740bcedac141
Chris Willis <cmwillis@google.com>
2022-03-16 01:16:49 +0000

    works for n images in metadata JSON

--- main.py
+++ main.py
@@ -44,21 +44,6 @@ def process_image(event, context):
     metadata_json_blob.download_to_filename(local_json_filepath)
     print("Successfully downloaded " + local_json_filepath)
 
-    # Test: print JSON file contents to logs
-    # todo: dump to string and replace newlines with spaces
-    # with open(local_json_filepath, "r") as json_file:
-    #     print(json_file.read())
-
-    # Send the referenced image to SVAI to get back a list of item IDs found 
-    # in the image and update file at local_json_filepath
-    # todo: dont have the file open for 5 million years
-    with open(local_json_filepath,'r') as route_json_file:
-        route_json_data = json.load(route_json_file)
-        
-    # todo: support multiple image files        
-    image_file_location = route_json_data["data"][0]["href"]
-    print("Sending " + image_file_location + " to SVAI to extract item IDs from image")
-
     # Get access token to call SVAI API
     print("Acquiring access token")
     auth_url = ('http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token')
@@ -68,44 +53,52 @@ def process_image(event, context):
     # print('access_token: ', access_token)
     print("Access token acquired")
 
-    # Call SVAI API
-    print("Calling the SVAI API")
-    svai_project_number = 903129578520
-    url = ('https://aistreams.googleapis.com/v1alpha1/projects/'
-            f'{svai_project_number}/locations/us-central1/'
-            'clusters:predictShelfHealth?')
-
-    # todo: loop through sample_data/image_metadata.json and process multiple images from the route
-    data = json.dumps({
-        "camera_id":"1001",
-        "input_image": {
-            "image_gcs_uri": "gs://route_images_02/82ca8d2495d0b4a0f643128f9391383a61ec39d48ea4101d994d422e7799bcad6ad34e303aff29ffa5455e935c18b92e9cc7767c0263e5655439b01c8317fa4f.jpg"
-        },
-        "config": {
-            "processor_name": "projects/626086442885/locations/us/processors/1aac1c81b63cefc1",
-            "price_tag_detection_model": "projects/617321834341/locations/us-central1/endpoints/2885725441702756352"
-        },
-        "analysis_type": "ANALYSIS_TYPE_PRICE_TAG_RECOGNITION"
-    })
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Content-Type': 'application/json; charset=utf-8'
-    }
-    response = requests.post(url, headers=headers, data=data)
-    response_metadata = {
-        'json': response.json(),
-        'time': datetime.datetime.now(),
-    }
-    print('Response json: ', json.dumps(response_metadata['json']))
-
-    # Pull item IDs out of SVAI response and add them to the route JSON
-    print("Appending item IDs from SVAI response to original image metadata JSON")
-    svai_response_dict = json.loads(json.dumps(response_metadata['json']),strict=False)
-    items_dict = []
-    for i in range(len(svai_response_dict["priceTags"])):
-        items_dict.append(svai_response_dict["priceTags"][i]["entities"][0]["normalizedTextValue"])
-    route_json_data["data"][0]["items"] = items_dict
+    # Send the referenced image to SVAI to get back a list of item IDs found 
+    # in the image and update file at local_json_filepath
+    with open(local_json_filepath,'r') as route_json_file:
+        route_json_data = json.load(route_json_file)
     
+    for item in route_json_data["data"]:
+        image_file_location = item["href"]
+        print("Sending " + image_file_location + " to SVAI to extract item IDs from image")
+
+        # Call SVAI API
+        print("Calling the SVAI API")
+        svai_project_number = 903129578520
+        url = ('https://aistreams.googleapis.com/v1alpha1/projects/'
+                f'{svai_project_number}/locations/us-central1/'
+                'clusters:predictShelfHealth?')
+
+        data = json.dumps({
+            "camera_id":"1001",
+            "input_image": {
+                "image_gcs_uri": image_file_location
+            },
+            "config": {
+                "processor_name": "projects/626086442885/locations/us/processors/1aac1c81b63cefc1",
+                "price_tag_detection_model": "projects/617321834341/locations/us-central1/endpoints/2885725441702756352"
+            },
+            "analysis_type": "ANALYSIS_TYPE_PRICE_TAG_RECOGNITION"
+        })
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+        response = requests.post(url, headers=headers, data=data)
+        response_metadata = {
+            'json': response.json(),
+            'time': datetime.datetime.now(),
+        }
+        print('Response json: ', json.dumps(response_metadata['json']))
+
+        # Pull item IDs out of SVAI response and add them to the route JSON
+        print("Appending item IDs from SVAI response to original image metadata JSON")
+        svai_response_dict = json.loads(json.dumps(response_metadata['json']),strict=False)
+        items_dict = []
+        for i in range(len(svai_response_dict["priceTags"])):
+            items_dict.append(svai_response_dict["priceTags"][i]["entities"][0]["normalizedTextValue"])
+        item["items"] = items_dict
+
     # results JSON file must all be on one line in order for BQ autodetect
     # schema to work
     route_json_file = open(local_json_filepath, 'w')
